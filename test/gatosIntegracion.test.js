4e136fca573497ad245985eaa40ae43cf0ee92a1
Jose Antonio Pozo Gonzalez <iwc70842@educastur.es>
2025-05-04 22:35:04 +0200

    Modificación y mejora de los test de integración

--- test/gatosIntegracion.test.js
+++ test/gatosIntegracion.test.js
@@ -1,12 +1,36 @@
 // Importación de dependencias
 const request = require('supertest');
-const app = require('../app'); 
+const app = require('../app');
 
-describe('GET /gatos', () => {
-  it('Listar los gatos correctamente', async () => {
-    
-    const response = await request(app).get('/gatos');       
-    expect(response.status).toBe(200);  
+// Datos de prueba
+const mockGato = require('../data/gato.json');
 
+
+describe('Integración /gatos métodos GET, PUT y DELETE', () => {
+
+  test('GET /gatos - Listar todos los gatos', async () => {
+    const response = await request(app).get('/gatos');
+    expect(response.status).toBe(200);
   });
+
+  test('GET /gatos/:id - Recuperar gato por ID', async () => {
+    const response = await request(app).get(`/gatos/${mockGato.id}`);
+    expect(response.status).toBe(200);
+  });
+
+  test('PUT /gatos/:id - Actualizar un gato existente', async () => {
+    const gatoActualizado = { ...mockGato, colores: 'Gris perla' };
+
+    const response = await request(app)
+      .put(`/gatos/${mockGato.id}`)
+      .send(gatoActualizado);
+
+    expect(response.status).toBe(200);    
+  });
+
+  test('DELETE /gatos/:id - Eliminar un gato existente', async () => {
+    const response = await request(app).delete(`/gatos/${mockGato.id}`);
+    expect(response.status).toBe(200);
+  });
+
 });
